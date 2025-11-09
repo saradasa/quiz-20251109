1a8c5cdefcb6547bd8ed7388b69df79f1243d25a
saradasa <y.takumi0912abc@gmail.com>
2025-11-09 22:13:29 +0900

    add: クイズの作成

--- app/quiz/page.jsx
+++ app/quiz/page.jsx
@@ -1,8 +1,39 @@
+"use client";
 import Quiz from "next"
+import { useState } from "react";
+import { quizData } from "../../data";
+import Style from "./quiz.module.css"
+
 export default function Question(){
+    const [quizId,setQuizId] = useState(0);
+    const [selected, setSelected] = useState("");
+    const [result, setResult] = useState(null);
+
+    const handleAnswer = (option) => {
+    setSelected(option);
+    if (option === quizId.answer) {
+      setResult("正解！🎉");
+    } else {
+      setResult("不正解...💦");
+    }
+
+
+  };
     return(
         <>
-        aaa
+        <div>{quizData[quizId].question}</div>
+        <div className={Style.containar}>{quizData[quizId].options.map((option,index) => (
+            <button className={Style.button} 
+                key={index}
+                onClick={() => handleAnswer(option)}
+                disabled={!!result}
+            >{option}</button>
+        ))}</div>
+        {result && (
+          <p className={result.includes("正解") ? "green" : "red"}>
+            {result}
+          </p>
+        )}
         </>
     )
 }
---
+++ data.js
@@ -0,0 +1,23 @@
+// クイズデータを配列で定義
+export const quizData = [
+  {
+    question: "太陽系で一番大きい惑星はどれ？",
+    options: ["地球", "木星", "火星", "金星"],
+    answer: "木星"
+  },
+  {
+    question: "日本の首都はどこ？",
+    options: ["大阪", "東京", "京都", "名古屋"],
+    answer: "東京"
+  },
+  {
+    question: "水の化学式はどれ？",
+    options: ["H2O", "CO2", "O2", "NaCl"],
+    answer: "H2O"
+  },
+  {
+    question: "次のうち哺乳類はどれ？",
+    options: ["カエル", "ヘビ", "イルカ", "スズメ"],
+    answer: "イルカ"
+  }
+];
